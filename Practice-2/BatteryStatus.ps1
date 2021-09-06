ca90c27f22d7598f49e091d3cedce0960187db7a
jmorin8 <javier.morin_ortiz@hotmail.com>
2021-09-06 02:50:46 -0500

    Uploaded powerShell script

---
+++ Practice-2/BatteryStatus.ps1
@@ -0,0 +1,23 @@
+﻿function BatteryStatus{
+    param([string] $computer)
+
+    $BatteryStatus = (Get-WmiObject -class win32_battery -ComputerName $computer).BatteryStatus # Check if batterry is being charging or not 
+        
+    if($BatteryStatus -eq 2){ # status = 1 --> battery is not being charged || 2 --> battery is being charged
+        :test while($BatteryStatus -lt "100"){
+            $ChargeRemaining = (Get-WmiObject -class win32_battery -ComputerName $computer).EstimatedChargeRemaining
+            
+            if($ChargeRemaining -eq "100"){
+                $msg = [System.Windows.Forms.MessageBox]::Show("Full charged battery","Advice",[System.Windows.Forms.MessageBoxButtons]::OK) # Display message box to user
+                
+                break test #end while loop
+            }
+            
+            sleep -Seconds 900 # Script will sleep 15 minutes
+        }
+    }
+}
+
+
+$computer = "localhost"
+BatteryStatus($computer)
